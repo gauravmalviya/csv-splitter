4e7e1c45e0bf3597648f71418eec1d9f8d9510ec
Gaurav <gaurav.malviya8@gmail.com>
2018-11-04 05:18:49 +0530

    intial commit

---
+++ FileSplitter/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace FileSplitter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.txtInputURL = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnOutput = new System.Windows.Forms.Button();
+            this.txtLineNumber = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(520, 84);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(75, 23);
+            this.btnSelect.TabIndex = 0;
+            this.btnSelect.Text = "Select File";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // txtInputURL
+            // 
+            this.txtInputURL.Location = new System.Drawing.Point(161, 86);
+            this.txtInputURL.Name = "txtInputURL";
+            this.txtInputURL.Size = new System.Drawing.Size(353, 20);
+            this.txtInputURL.TabIndex = 1;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(161, 34);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(353, 20);
+            this.txtOutput.TabIndex = 3;
+            // 
+            // btnOutput
+            // 
+            this.btnOutput.Location = new System.Drawing.Point(520, 32);
+            this.btnOutput.Name = "btnOutput";
+            this.btnOutput.Size = new System.Drawing.Size(75, 23);
+            this.btnOutput.TabIndex = 2;
+            this.btnOutput.Text = "Output";
+            this.btnOutput.UseVisualStyleBackColor = true;
+            this.btnOutput.Click += new System.EventHandler(this.btnOutput_Click);
+            // 
+            // txtLineNumber
+            // 
+            this.txtLineNumber.Location = new System.Drawing.Point(161, 60);
+            this.txtLineNumber.Name = "txtLineNumber";
+            this.txtLineNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtLineNumber.TabIndex = 4;
+            this.txtLineNumber.Text = "2000";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtLineNumber);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnOutput);
+            this.Controls.Add(this.txtInputURL);
+            this.Controls.Add(this.btnSelect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.TextBox txtInputURL;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnOutput;
+        private System.Windows.Forms.TextBox txtLineNumber;
+    }
+}
+
---
+++ FileSplitter/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileSplitter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            int _splitRowCount = int.Parse(txtLineNumber.Text);
+            OpenFileDialog op1 = new OpenFileDialog();
+
+            op1.Multiselect = true;
+
+            op1.ShowDialog();
+
+            op1.Filter = "allfiles|*.csv";
+            txtInputURL.Text = op1.FileName;
+            int count = 0;
+            string[] FName;
+            foreach (string s in op1.FileNames)
+            {
+                
+                FName = s.Split('\\');
+                //File.Copy(s, txtOutput.Text +"\\" + FName[FName.Length - 1]);
+
+
+                var headerLine = File.ReadLines(txtInputURL.Text);
+
+                var nHeaderLine = headerLine.Take(1);
+                string FHeader = nHeaderLine.ToString();
+
+
+                string[] lines = File.ReadAllLines(txtInputURL.Text);
+
+                int _rowCountOutput = 1;
+                int _rowCount = 1;
+                bool _isHeader = false;
+                string firstHeader = "";
+                List<string> fileRow = new List<string>(); ;
+                foreach (string _row  in lines)
+                {
+                    if (_rowCount == 1 && _isHeader == false)
+                    {
+                        firstHeader = _row;
+                        fileRow.Add(_row);
+                    }
+                    else
+                    {
+                        fileRow.Add(_row);
+                    }
+
+                    if (_rowCount == _splitRowCount)
+                    {   
+                        File.WriteAllLines(txtOutput.Text +"\\" + _rowCountOutput + ".csv", fileRow.ToArray());
+                        _rowCountOutput++;
+                        fileRow = new List<string>();
+                        fileRow.Add(firstHeader);
+                        _rowCount = 0;
+                    }
+                    if (_isHeader == false)
+                        _isHeader = true;
+
+                    _rowCount++;
+                }
+
+
+                count++;
+            }
+
+            
+
+
+
+
+
+            MessageBox.Show(Convert.ToString(count) + " File(s) copied");
+
+        }
+
+        private void btnOutput_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            folderDlg.ShowNewFolderButton = true;
+            // Show the FolderBrowserDialog.  
+            DialogResult result = folderDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtOutput.Text = folderDlg.SelectedPath;
+                Environment.SpecialFolder root = folderDlg.RootFolder;
+            }
+        }
+
+    }
+}
