cdb5ac5d02ed73767646f14934e9c7d432fc9157
Gaurav <gaurav.malviya8@gmail.com>
2018-11-04 05:52:13 +0530

    UI Changes

---
+++ CSVSplitter/frmCSV.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace FileSplitter
+{
+    partial class frmCSV
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.txtInputURL = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnOutput = new System.Windows.Forms.Button();
+            this.txtLineNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(361, 89);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(75, 22);
+            this.btnSelect.TabIndex = 0;
+            this.btnSelect.Text = "Select";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // txtInputURL
+            // 
+            this.txtInputURL.Enabled = false;
+            this.txtInputURL.Location = new System.Drawing.Point(136, 90);
+            this.txtInputURL.Name = "txtInputURL";
+            this.txtInputURL.Size = new System.Drawing.Size(228, 20);
+            this.txtInputURL.TabIndex = 1;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Enabled = false;
+            this.txtOutput.Location = new System.Drawing.Point(136, 36);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(228, 20);
+            this.txtOutput.TabIndex = 3;
+            // 
+            // btnOutput
+            // 
+            this.btnOutput.Location = new System.Drawing.Point(361, 35);
+            this.btnOutput.Name = "btnOutput";
+            this.btnOutput.Size = new System.Drawing.Size(75, 22);
+            this.btnOutput.TabIndex = 2;
+            this.btnOutput.Text = "Select";
+            this.btnOutput.UseVisualStyleBackColor = true;
+            this.btnOutput.Click += new System.EventHandler(this.btnOutput_Click);
+            // 
+            // txtLineNumber
+            // 
+            this.txtLineNumber.Location = new System.Drawing.Point(136, 64);
+            this.txtLineNumber.Name = "txtLineNumber";
+            this.txtLineNumber.Size = new System.Drawing.Size(84, 20);
+            this.txtLineNumber.TabIndex = 4;
+            this.txtLineNumber.Text = "2000";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Output Folder :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Split on Line Number\'s :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Select CSV File :";
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(361, 155);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(75, 22);
+            this.btnProcess.TabIndex = 8;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // frmCSV
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 189);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLineNumber);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnOutput);
+            this.Controls.Add(this.txtInputURL);
+            this.Controls.Add(this.btnSelect);
+            this.MaximizeBox = false;
+            this.Name = "frmCSV";
+            this.Text = "CSV-Splitter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.TextBox txtInputURL;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnOutput;
+        private System.Windows.Forms.TextBox txtLineNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnProcess;
+    }
+}
+
--- CSVSplitter/frmCSV.cs
+++ CSVSplitter/frmCSV.cs
@@ -11,84 +11,20 @@ using System.Windows.Forms;
 
 namespace FileSplitter
 {
-    public partial class Form1 : Form
+    public partial class frmCSV : Form
     {
-        public Form1()
+        public frmCSV()
         {
             InitializeComponent();
         }
 
         private void btnSelect_Click(object sender, EventArgs e)
         {
-            int _splitRowCount = int.Parse(txtLineNumber.Text);
             OpenFileDialog op1 = new OpenFileDialog();
-
             op1.Multiselect = true;
-
             op1.ShowDialog();
-
             op1.Filter = "allfiles|*.csv";
             txtInputURL.Text = op1.FileName;
-            int count = 0;
-            string[] FName;
-            foreach (string s in op1.FileNames)
-            {
-                
-                FName = s.Split('\\');
-                //File.Copy(s, txtOutput.Text +"\\" + FName[FName.Length - 1]);
-
-
-                var headerLine = File.ReadLines(txtInputURL.Text);
-
-                var nHeaderLine = headerLine.Take(1);
-                string FHeader = nHeaderLine.ToString();
-
-
-                string[] lines = File.ReadAllLines(txtInputURL.Text);
-
-                int _rowCountOutput = 1;
-                int _rowCount = 1;
-                bool _isHeader = false;
-                string firstHeader = "";
-                List<string> fileRow = new List<string>(); ;
-                foreach (string _row  in lines)
-                {
-                    if (_rowCount == 1 && _isHeader == false)
-                    {
-                        firstHeader = _row;
-                        fileRow.Add(_row);
-                    }
-                    else
-                    {
-                        fileRow.Add(_row);
-                    }
-
-                    if (_rowCount == _splitRowCount)
-                    {   
-                        File.WriteAllLines(txtOutput.Text +"\\" + _rowCountOutput + ".csv", fileRow.ToArray());
-                        _rowCountOutput++;
-                        fileRow = new List<string>();
-                        fileRow.Add(firstHeader);
-                        _rowCount = 0;
-                    }
-                    if (_isHeader == false)
-                        _isHeader = true;
-
-                    _rowCount++;
-                }
-
-
-                count++;
-            }
-
-            
-
-
-
-
-
-            MessageBox.Show(Convert.ToString(count) + " File(s) copied");
-
         }
 
         private void btnOutput_Click(object sender, EventArgs e)
@@ -104,5 +40,54 @@ namespace FileSplitter
             }
         }
 
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            if(txtInputURL.Text!="")
+            {
+                int _splitRowCount = int.Parse(txtLineNumber.Text);
+                int count = 0;
+                string[] FName;
+                FName = txtInputURL.Text.Split('\\');                
+                var headerLine = File.ReadLines(txtInputURL.Text);
+                var nHeaderLine = headerLine.Take(1);
+                string FHeader = nHeaderLine.ToString();
+                string[] lines = File.ReadAllLines(txtInputURL.Text);
+                int _rowCountOutput = 1;
+                int _rowCount = 1;
+                bool _isHeader = false;
+                string firstHeader = "";
+                List<string> fileRow = new List<string>(); ;
+                foreach (string _row in lines)
+                {
+                    if (_rowCount == 1 && _isHeader == false)
+                    {
+                        firstHeader = _row;
+                        fileRow.Add(_row);
+                    }
+                    else
+                    {
+                        fileRow.Add(_row);
+                    }
+                    if (_rowCount == _splitRowCount)
+                    {
+                        File.WriteAllLines(txtOutput.Text + "\\" + _rowCountOutput + ".csv", fileRow.ToArray());
+                        _rowCountOutput++;
+                        fileRow = new List<string>();
+                        fileRow.Add(firstHeader);
+                        _rowCount = 0;
+                    }
+                    if (_isHeader == false)
+                        _isHeader = true;
+
+                    _rowCount++;
+                }
+
+
+                count++;
+            }
+            MessageBox.Show("File Process Sucessfully.");
+
+
+        }
     }
 }
